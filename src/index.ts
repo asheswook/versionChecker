ab5b6cd6b91374c227471d67cf32478ce6fbae80
asheswook <wookboy00@naver.com>
2023-02-28 16:51:27 +0900

    Feat: add uSAINT checker

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,48 @@
+import type { Request as req, ExecutionContext } from "@cloudflare/workers-types/experimental";
+
 export default {
-  async fetch(request, env) {
-    try {
-      const { pathname } = new URL(request.url);
+    async fetch(request: req, env: any, ctx: ExecutionContext) {
+        try {
+            const { pathname } = new URL(request.url);
 
-      if (pathname.startsWith("/status")) {
-        const httpStatusCode = Number(pathname.split("/")[2]);
+            if (pathname === "/check/uSAINT") return uSAINTChecker(request, env.GITHUB_TOKEN);
 
-        return Number.isInteger(httpStatusCode)
-          ? fetch("https://http.cat/" + httpStatusCode)
-          : new Response("That's not a valid HTTP status code.");
-      }
-      return fetch("https://welcome.developers.workers.dev");
-    } catch(e) {
-      return new Response(err.stack, { status: 500 })
+            return new Response("Not found", { status: 404 });
+        } catch (e: any) {
+            return new Response(e.stack, { status: 500 });
+        }
+    },
+};
+
+const uSAINTChecker = async (request: req, token: string) => {
+    const resp = await fetch("https://api.github.com/repos/asheswook/uSAINT-macOS/releases/latest", {
+        headers: {
+            Accept: "application/vnd.github+json",
+            Authorization: `Bearer ${token}`,
+            "User-Agent": "uSAINT-macOS",
+        },
+    });
+
+    const results = await gatherResponse(resp);
+
+    const data = {
+        latestVersion: JSON.parse(results).tag_name,
+    };
+
+    const json = JSON.stringify(data, null, 2);
+
+    return new Response(json, {
+        headers: {
+            "content-type": "application/json;charset=UTF-8",
+        },
+    });
+};
+
+const gatherResponse = async (response: Response) => {
+    const { headers } = response;
+    const contentType = headers.get("content-type") || "";
+    if (contentType.includes("application/json")) {
+        return JSON.stringify(await response.json());
     }
-  }
-}
+    return response.text();
+};
